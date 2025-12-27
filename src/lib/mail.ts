1828dae6419436951e9dfd135a3a5f5ba47acf7f
whoisfazenda? <86470948+whoisfazenda@users.noreply.github.com>
2025-12-27 22:16:02 +0200

    upd

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -6,7 +6,7 @@ const PRIMARY_COLOR = "#7c3aed";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // true for 465, false for other ports
+  secure: Number(process.env.SMTP_PORT) === 465, // true for 465, false for 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
